aa3453380f7ec8f51f8bf04e8ed23d262ffd7152
deems111 <56789481+deems111@users.noreply.github.com>
2019-11-07 22:46:37 +0300

    Bug Fix
    Bug Fix

--- src/main/java/sberMain/entity/Account.java
+++ src/main/java/sberMain/entity/Account.java
@@ -1,53 +1,27 @@
 package sberMain.entity;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
-@Table(name = "ACCOUNT")
+@Table(name = "ACCOUNT", schema = "public")
 public class Account {
 
-    @Column()
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    private String id;
+
+    @Column(nullable = false)
     private String balance;
 
-    @Column()
+    @Column(nullable = false, unique = true)
     private String accountNumber;
 
-    @Column()
-    private String inn;
-
-    @ManyToOne()
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "inn")
     private Client client;
-
-    public String getBalance() {
-        return balance;
-    }
-
-    public void setBalance(String balance) {
-        this.balance = balance;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public Client getClient() {
-        return client;
-    }
-
-    public void setClient(Client client) {
-        this.client = client;
-    }
-
-    public String getInn() {
-        return inn;
-    }
-
-    public void setInn(String inn) {
-        this.inn = inn;
-    }
 }
--- src/main/java/sberMain/entity/Client.java
+++ src/main/java/sberMain/entity/Client.java
@@ -1,51 +1,25 @@
 package sberMain.entity;
 
+import lombok.*;
 import sberMain.entity.Account;
 
 import javax.persistence.*;
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
-@Table(name = "CLIENT")
+@Table(name = "CLIENT", schema = "public")
 public class Client {
 
     @Id
-    @Column()
+    @Column(name = "inn", nullable = false, unique = true)
     private String inn;
 
-    @Column()
+    @Column(nullable = false)
     private String name;
 
-    @OneToMany(mappedBy = "client")
-    private List<Account> accounts;
-
-    public Client() {}
-
-    public Client(String inn) {
-        this.inn = inn;
-    }
-
-    public String  getInn() {
-        return inn;
-    }
-
-    public void setInn(String inn) {
-        this.inn = inn;
-    }
-
-    public List<Account> getAccounts() {
-        return accounts;
-    }
-
-    public void setAccounts(List<Account> accounts) {
-        this.accounts = accounts;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Account> account;
 }
